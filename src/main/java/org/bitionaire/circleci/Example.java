3ade26ea4427ca5eb8a16532e0db95694f8cd2e6
bitionaire <info@bitionaire.org>
2018-06-16 16:39:39 +0200

    Added project stub

---
+++ src/main/java/org/bitionaire/circleci/Example.java
@@ -0,0 +1,9 @@
+package org.bitionaire.circleci;
+
+public class Example {
+
+    public boolean isThisReal() {
+        return true;
+    }
+
+}
---
+++ src/test/java/org/bitionaire/circleci/ExampleTest.java
@@ -0,0 +1,13 @@
+package org.bitionaire.circleci;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    @Test
+    public void isThisReal() {
+        assertEquals(true, new Example().isThisReal());
+    }
+}
